178c47ae5aa2e9f9f4b8f95850dcf16e9e9be8a7
SebastiaanWouters <sebastiaan.wouters@kuleuven.be>
2024-08-26 12:20:01 +0200

    add counter to confetti page

--- resources/js/vue/Confetti.vue
+++ resources/js/vue/Confetti.vue
@@ -1,9 +1,16 @@
 <script setup>
-  import ConfettiExplosion from "vue-confetti-explosion";
+import ConfettiExplosion from "vue-confetti-explosion";
+import {ref} from "vue";
+
+const counter = ref(0);
 </script>
 
 <template>
-    <div style="display: flex; justify-content: center;">
-        <ConfettiExplosion :stageHeight="620" />
-    </div>
+  <div style="display: flex; justify-content: center;">
+    <ConfettiExplosion :stageHeight="620"/>
+  </div>
+  <div style="font-weight: 800; margin-block: 1rem;">Vue</div>
+  <button @click="counter++">Increment</button>
+  <button @click="counter--">Decrement</button>
+  <p>{{ counter }}</p>
 </template>
