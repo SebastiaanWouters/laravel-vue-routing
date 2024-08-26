e1d7750d592982a597d1fba054aa6b90b26cbc53
Sebastiaan Wouters <24827662+SebastiaanWouters@users.noreply.github.com>
2024-08-26 11:01:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '100.86.47.120',
+        hmr: {
+            host: '100.86.47.120'
+        }
+    },
 });
