199071b9fb98660d810828d71abb53c49c2bb45c
SebastiaanWouters <sebastiaan.wouters@kuleuven.be>
2024-08-26 12:06:41 +0200

    limit confetti height

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,20 +1,30 @@
 @props(['title' => 'Routing'])
 
 <html>
-    <head>
-        <title>{{ $title }}</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <h1>Server Routing</h1>
-        <div>
-            <a href="/">Home</a>
-            <a href="/1">1</a>
-            <a href="/2">2</a>
-        </div>
-        <hr/>
-        {{ $slot }}
-    </body>
+<head>
+    <title>{{ $title }}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+    >
+    <style>
+        button {
+            margin-right: 0.5rem;
+            margin-bottom: 0.5rem;
+        }
+    </style>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body style="padding: 1rem;">
+<h1>Server Routing</h1>
+<div style="display: flex; gap: 0.75rem;">
+    <a href="/">Home</a>
+    <a href="/1">1</a>
+    <a href="/2">2</a>
+</div>
+<hr/>
+{{ $slot }}
+</body>
 </html>
