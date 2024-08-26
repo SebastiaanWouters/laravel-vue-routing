107a511b0378e4c9b54f7449e88d2bd79df80415
SebastiaanWouters <sebastiaan.wouters@kuleuven.be>
2024-08-26 11:26:03 +0200

    add global counter controls to confetti

--- resources/js/vue/Confetti.vue
+++ resources/js/vue/Confetti.vue
@@ -13,7 +13,9 @@
     <div style="display: flex; justify-content: center;">
         <ConfettiExplosion />
     </div>
-     <div style="font-weight: 800; margin-block: 1rem;">Vue Global Counter</div>
+    <div style="font-weight: 800; margin-block: 1rem;">Vue Global Counter</div>
+    <button @click="globalCounter++">Increment</button>
+    <button @click="globalCounter--">Decrement</button>
     <p>{{globalCounter}}</p>
     <p>{{props.message}}</p>
 </template>
