fd21723d749effa7d8fa3e723411cc0d2b2311ae
SebastiaanWouters <sebastiaan.wouters@kuleuven.be>
2024-08-26 12:11:38 +0200

    add global counter controls

--- resources/js/vue/Confetti.vue
+++ resources/js/vue/Confetti.vue
@@ -1,19 +1,21 @@
 <script setup>
-  import ConfettiExplosion from "vue-confetti-explosion";
-  import { useGlobalCounter } from "../store";
+import ConfettiExplosion from "vue-confetti-explosion";
+import {useGlobalCounter} from "../store";
 
-  const props = defineProps({
-        message: { default: 'Hello Vue!' }
-    })
+const props = defineProps({
+  message: {default: 'Hello Vue!'}
+})
 
-  const globalCounter = useGlobalCounter();
+const globalCounter = useGlobalCounter();
 </script>
 
 <template>
-    <div style="display: flex; justify-content: center;">
-        <ConfettiExplosion stageHeight="620" />
-    </div>
-     <div style="font-weight: 800; margin-block: 1rem;">Vue Global Counter</div>
-    <p>{{globalCounter}}</p>
-    <p>{{props.message}}</p>
+  <div style="display: flex; justify-content: center;">
+    <ConfettiExplosion stageHeight="620"/>
+  </div>
+  <div style="font-weight: 800; margin-block: 1rem;">Vue Global Counter</div>
+  <button @click="globalCounter++">Increment</button>
+  <button @click="globalCounter--">Decrement</button>
+  <p>{{ globalCounter }}</p>
+  <p>{{ props.message }}</p>
 </template>
