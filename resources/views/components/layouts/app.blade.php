7e959b06f930283f868fa18844db0281b0adcbca
SebastiaanWouters <sebastiaan.wouters@kuleuven.be>
2024-08-26 11:48:54 +0200

    add pico css

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,11 +5,15 @@
         <title>{{ $title }}</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+        >
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body>
+    <body style="padding: 1rem;">
         <h1>Server Routing</h1>
-        <div>
+        <div style="display: flex; gap: 0.75rem;">
             <a href="/">Home</a>
             <a href="/1">1</a>
             <a href="/2">2</a>
